6c081c2011520c6887ed26f6d31d3137dc2c2353
ehharvey <36806437+ehharvey@users.noreply.github.com>
2022-11-14 23:38:01 -0500

    Initial

---
+++ my_website/assets/js/lunr/lunr-store.js
@@ -0,0 +1,7 @@
+var store = [{
+        "title": "Automated VM deployment using Ansible and LXD",
+        "excerpt":"Summary I used Ansible to configure some spare PCs These PCs run VMs and containers using LXD All devices (bare-metal and virtualized) use TailScale to provide me with a flat (and secure) network For web-accessible resources, I use Cloudflare Tunnels to expose self-hosted applications All the above occur with zero...","categories": ["ansible"],
+        "tags": [],
+        "url": "/ansible/2022/11/14/automated-vm-deployment.html",
+        "teaser": null
+      }]
